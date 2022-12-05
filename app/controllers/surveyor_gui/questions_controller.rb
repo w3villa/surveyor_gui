5a8aeda1a7a89a5f168a25f087628df4792e29fe
deepanshu-w3villa <deepanshu.kashyap@w3villa.com>
2022-12-05 23:22:32 +0530

    fix braking of create question

--- app/controllers/surveyor_gui/questions_controller.rb
+++ app/controllers/surveyor_gui/questions_controller.rb
@@ -56,7 +56,9 @@ class SurveyorGui::QuestionsController < ApplicationController
         #load any page - if it has no flash errors, the colorbox that contains it will be closed immediately after the page loads
         # render :inline => '<div id="cboxQuestionId">'+@question.id.to_s+'</div>', :layout => 'surveyor_gui/surveyor_gui_blank'
 
-        redirect_to "/companies/courses/#{params[:question][:course_id]}/lessons/#{params[:question][:lesson_id]}/topics/#{params[:question][:topic_id]}/topic-quiz/edit/#{params[:question][:survey_section_id]}"
+        redirect_to surveyor_gui.edit_surveyform_url(@question.survey_section.survey)
+
+        # redirect_to "/companies/courses/#{params[:question][:course_id]}/lessons/#{params[:question][:lesson_id]}/topics/#{params[:question][:topic_id]}/topic-quiz/edit/#{params[:question][:survey_section_id]}"
 
       else
         @title = "Add Question"
